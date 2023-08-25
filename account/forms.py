d4d4eb90d953979db1f2b046794e09e7046d4f5d
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-08-25 21:31:59 +0200

    add own authentication backend with email

--- account/forms.py
+++ account/forms.py
@@ -31,7 +31,38 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name', 'email']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('This email is occupied.')
+        return data
+    
+
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['date_of_birth', 'photo']
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password']:
+            raise forms.ValidatonError('Passwords aren\'t the same')
+        return cd['password']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise.forms.ValidationError('This email adress is already in use.')
+        return data
