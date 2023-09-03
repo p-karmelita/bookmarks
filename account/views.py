6f5be176737ca35872f6f6ce9231408c137c8c62
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-09-03 12:41:36 +0200

    avoid duplicated actions in activity stream

--- account/views.py
+++ account/views.py
@@ -66,19 +66,20 @@ def edit(request):
         user_form = UserEditForm(instance=request.user,
                                  data=request.POST)
         profile_form = ProfileEditForm(
-            instance=request.user.profile,
-            data=request.POST,
-            files=request.FILES)
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Profile updated succesfully')
+            messages.success(request, 'Profile updated '\
+                                      'successfully')
         else:
-            messages.error(request, 'An error occured during udpating profile.')
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
-
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile)
     return render(request,
                   'account/edit.html',
                   {'user_form': user_form,
