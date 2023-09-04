bd5ee2b92188739445b5162d10bc9ac2bba59119
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-09-04 18:33:24 +0200

    integrate project with redis in order to store views for image

--- images/views.py
+++ images/views.py
@@ -7,9 +7,16 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, \
                                   PageNotAnInteger
+from django.conf import settings
 from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
+import redis
+
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 @login_required
@@ -39,10 +46,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                   'total_views': total_views})
 
 
 @login_required
